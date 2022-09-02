ef53de0c3cd8b933bfcd841d900402d866df5184
EvitanRelta <shauntanzongzhi@gmail.com>
2022-09-03 01:35:03 +0800

    Add whitespace escaping

--- src/core/textProcesses/htmlEscapeSpaces.ts
+++ src/core/textProcesses/htmlEscapeSpaces.ts
@@ -4,9 +4,12 @@ import { applyReplacement, ReplacementArray } from './helpers/applyReplacement'
 const spaceHtmlEscapings: ReplacementArray = [
     ['\u2003', '&emsp;'],
     ['\u2002', '&ensp;'],
-    ['\u00A0', '&nbsp;'],
     ['\u2009', '&thinsp;'],
     ['\u200A', '&hairsp;'],
+
+    // Converts non-breaking space to space for escaping later in `escapeWhitespace`.
+    // `escapeWhitespace` will escape leading/trailer/repeated spaces into "&nbsp;".
+    ['\u00A0', ' '],
 ]
 
 export const htmlEscapeSpaces: TextProcess = (text) => {
