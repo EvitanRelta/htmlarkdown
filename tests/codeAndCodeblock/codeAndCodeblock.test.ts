6c774bb38426389589b4cac2f35a85b5b9ac0d26
EvitanRelta <shauntanzongzhi@gmail.com>
2022-09-16 21:55:40 +0800

    Add tests for code

---
+++ tests/codeAndCodeblock/codeAndCodeblock.test.ts
@@ -0,0 +1,11 @@
+import { HTMLarkdown } from '../../src'
+import { getTestPairHOF } from '../helpers'
+
+const getTestPair = getTestPairHOF(__dirname)
+const htmlarkdown = new HTMLarkdown()
+
+test('Code & Codeblock - code', () => {
+    const [htmlInput, expectedMarkdownOutput] = getTestPair('./code')
+    const outputMarkdown = htmlarkdown.convert(htmlInput)
+    expect(outputMarkdown).toBe(expectedMarkdownOutput)
+})
