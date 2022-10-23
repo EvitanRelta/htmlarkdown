2414fd1e00d891a9cc3fef3fcaf8e068b3f0b4ff
EvitanRelta <shauntanzongzhi@gmail.com>
2022-10-23 16:59:20 +0800

    Rename 'escaping' tests

--- tests/escaping/escaping.test.ts
+++ tests/escaping/escaping.test.ts
@@ -4,25 +4,25 @@ import { getTestPairHOF } from '../helpers'
 const getTestPair = getTestPairHOF(__dirname)
 const htmlarkdown = new HTMLarkdown()
 
-test('Escaping - basic markdown escapings', () => {
+test('Escaping - basic markdown', () => {
     const [htmlInput, expectedMarkdownOutput] = getTestPair('./basicMarkdown')
     const outputMarkdown = htmlarkdown.convert(htmlInput)
     expect(outputMarkdown).toBe(expectedMarkdownOutput)
 })
 
-test('Escaping - advanced markdown escapings', () => {
+test('Escaping - advanced markdown', () => {
     const [htmlInput, expectedMarkdownOutput] = getTestPair('./advancedMarkdown')
     const outputMarkdown = htmlarkdown.convert(htmlInput)
     expect(outputMarkdown).toBe(expectedMarkdownOutput)
 })
 
-test('Escaping - space HTML escapings', () => {
-    const [htmlInput, expectedMarkdownOutput] = getTestPair('./spaceHtml')
+test('Escaping - HTML-escaped spaces', () => {
+    const [htmlInput, expectedMarkdownOutput] = getTestPair('./htmlEscapedSpaces')
     const outputMarkdown = htmlarkdown.convert(htmlInput)
     expect(outputMarkdown).toBe(expectedMarkdownOutput)
 })
 
-test('Escaping - whitespace escapings', () => {
+test('Escaping - whitespace', () => {
     const [htmlInput, expectedMarkdownOutput] = getTestPair('./whitespace')
     const outputMarkdown = htmlarkdown.convert(htmlInput)
     expect(outputMarkdown).toBe(expectedMarkdownOutput)
