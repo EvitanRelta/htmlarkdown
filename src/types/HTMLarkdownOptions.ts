b3bde72deb134b2f2688319235688a6b3a122722
EvitanRelta <shauntanzongzhi@gmail.com>
2022-10-23 20:24:46 +0800

    Add default to JSDoc for 'urlTransformer' option

--- src/types/HTMLarkdownOptions.ts
+++ src/types/HTMLarkdownOptions.ts
@@ -1,6 +1,9 @@
 import type { UrlTransformer } from './UrlTransformer'
 
 export interface HTMLarkdownOptions {
-    /** Transforms the URLs of hyperlinks & images. */
+    /**
+     * Transforms the URLs of hyperlinks & images.
+     * @default null
+     */
     urlTransformer: null | UrlTransformer
 }
