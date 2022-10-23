c4a3f9b7474f9c1d6e5f4f6c8f139e5134bdadd0
EvitanRelta <shauntanzongzhi@gmail.com>
2022-10-23 21:53:47 +0800

    Abstract out br-tag-string for linebreaks

--- src/core/rules/linebreak.ts
+++ src/core/rules/linebreak.ts
@@ -1,12 +1,15 @@
 import type { Rule } from '../../types'
 import { hasOnlyLinebreaks, isHeading } from '../../utilities'
 
+export const BR_TAG_STR = '<br>'
+
 export const linebreak: Rule = {
     filter: 'br',
     replacement: (element) => {
         const parent = element.parentElement!
         const prefix = element.previousSibling === null || isHeading(parent) ? '' : '\n'
-        const content = element.nextSibling || hasOnlyLinebreaks(parent) ? '<br>' : '<br><br>'
+        const content =
+            element.nextSibling || hasOnlyLinebreaks(parent) ? BR_TAG_STR : BR_TAG_STR + BR_TAG_STR
         return prefix + content
     },
 }
--- src/core/rules/paragraph.ts
+++ src/core/rules/paragraph.ts
@@ -1,12 +1,13 @@
 import { any } from 'predicate-hof'
 import type { Rule } from '../../types'
 import { hasAnyOfAttributes, obeyForceHtml, toSanitisedHtmlHOF } from '../../utilities'
+import { BR_TAG_STR } from './linebreak'
 
 export const paragraph: Rule = {
     filter: ['p'],
     toUseHtmlPredicate: any(obeyForceHtml, hasAnyOfAttributes(['align'])),
     replacement: () => (innerContent) =>
-        (['', '<br>'].includes(innerContent) ? '<p><br></p>' : innerContent) + '\n\n',
+        (['', BR_TAG_STR].includes(innerContent) ? `<p>${BR_TAG_STR}</p>` : innerContent) + '\n\n',
     htmlReplacement: (element) => (innerContent) =>
-        toSanitisedHtmlHOF(element, ['align'])(innerContent || '<br>'),
+        toSanitisedHtmlHOF(element, ['align'])(innerContent || BR_TAG_STR),
 }
