7cd6990643c3fe7583c26ff61e510a07fb3b8272
EvitanRelta <shauntanzongzhi@gmail.com>
2022-12-07 19:30:00 +0800

    Trim extra newlines from block-elements in list-items

--- src/core/rules/listRules/listItem.ts
+++ src/core/rules/listRules/listItem.ts
@@ -1,6 +1,12 @@
 import { any } from 'predicate-hof'
 import type { Rule } from '../../../types'
-import { hasAnyOfAttributes, indent, obeyForceHtml, toSanitisedHtmlHOF } from '../../../utilities'
+import {
+    hasAnyOfAttributes,
+    indent,
+    obeyForceHtml,
+    toSanitisedHtmlHOF,
+    trimTrailingNewlines,
+} from '../../../utilities'
 
 export const listItem: Rule = {
     filter: ['li'],
@@ -8,13 +14,15 @@ export const listItem: Rule = {
     replacement: (element, __, parentOptions) => ({
         childOptions: { isInsideBlockElement: true },
         value: (innerContent) => {
+            const trimmedContent = trimTrailingNewlines(innerContent)
+
             if (!parentOptions.isOrderedList)
-                return `- ${indent(innerContent, 2).replace(/^ {2}/, '')}\n`
+                return `- ${indent(trimmedContent, 2).replace(/^ {2}/, '')}\n`
 
             const childIndex = Array.from(element.parentElement!.children).indexOf(element)
             const prefix = String(parentOptions.olStartingNum + childIndex)
             const indentSize = prefix.length + 2
-            return `${prefix}. ${indent(innerContent, indentSize).replace(
+            return `${prefix}. ${indent(trimmedContent, indentSize).replace(
                 new RegExp(`^ {${indentSize}}`),
                 ''
             )}\n`
