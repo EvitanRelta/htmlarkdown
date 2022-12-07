62a1c061b8479fc064163e55bc03e96caed7cf7f
EvitanRelta <shauntanzongzhi@gmail.com>
2022-12-08 00:17:32 +0800

    Make headings use HTML-syntax if they contain block elements

--- src/core/rules/heading.ts
+++ src/core/rules/heading.ts
@@ -1,10 +1,21 @@
 import { any } from 'predicate-hof'
 import type { RuleWithHTML } from '../../types'
-import { hasAnyOfAttributes, isHeading, obeyForceHtml, toSanitisedHtmlHOF } from '../../utilities'
+import {
+    hasAnyOfAttributes,
+    isBlock,
+    isHeading,
+    obeyForceHtml,
+    toSanitisedHtmlHOF,
+} from '../../utilities'
+
+const hasBlockElements = (element: Element): boolean => {
+    const children = Array.from(element.children)
+    return children.some(isBlock) || children.some(hasBlockElements)
+}
 
 export const heading: RuleWithHTML = {
     filter: isHeading,
-    toUseHtmlPredicate: any(obeyForceHtml, hasAnyOfAttributes(['align'])),
+    toUseHtmlPredicate: any(obeyForceHtml, hasAnyOfAttributes(['align']), hasBlockElements),
     replacement: (element) => (innerContent) => {
         const headingLevel = Number(element.tagName[1])
         const prefix = '#'.repeat(headingLevel)
